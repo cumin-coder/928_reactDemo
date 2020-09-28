087566516120b2816c6cd28c6e91d1ca1c373c1b
李子岚 <1254348304@qq.com>
2020-09-28 15:30:45 +0800

    updata

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React,{Component} from 'react';
-import {Route,Switch,Redirect} from "react-router-dom"
 
-import Login from "./pages/Login/login";
-import Admin from "./pages/Admin/admin";
 import './App.less';
 
 
 export default class App extends Component{
     render() {
         return (
-            <Switch>
-                <Route path="/login" component={Login}></Route>
-                <Route path="/admin" component={Admin}></Route>
-                <Redirect to="/login"></Redirect>
-            </Switch>
+         <div>App</div>
         )
     }
 
