aa7869d4b5118e58417238bcd11694f108f8a7db
李子岚 <1254348304@qq.com>
2020-09-28 16:39:29 +0800

    updata

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React,{Component} from 'react';
 
 import './App.less';
 
-
 export default class App extends Component{
     render() {
         return (
-         <div>Rudex</div>
+         <div>
+
+         </div>
         )
     }
 
